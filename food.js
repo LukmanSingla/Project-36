852888d5d849e8d00edcfc08ca8310dd8f8592cb
LukmanSingla <79403472+LukmanSingla@users.noreply.github.com>
2021-05-08 18:08:41 +0530

    Update food.js

--- food.js
+++ food.js
@@ -4,6 +4,8 @@ class Food{
     reference.on("value",function(data){
         petposition=data.val();
         foodN=petposition.food;
+        timeN=petposition.time;
+        a=petposition.time2;
     });
     pet = createSprite(800,250,10,10);
     pet.addAnimation("pet",petImg);
@@ -12,17 +14,7 @@ class Food{
     }
 
     display(){
-        if(petposition != undefined && game=="play") {if(keyDown(UP_ARROW)){
-            reference.set(
-                {
-                    food:foodN-1
-                }
-            );
-            pet.changeAnimation("pet",petImg);
-            game="wait";
-        }}
-
-
+        text("Last Fed : " + timeN + " " + a,100,100);
         if(game=="wait" && frameCount%60==0 ){
             pet.changeAnimation("pet1",petImg2);
             game="play"; 
@@ -46,11 +38,55 @@ class Food{
         for(var i=0;i<foodN;i++){
 
             if(i<10){
-                image(milk,i*50+60,250,20,30);
+                image(milk,i*50+60,250,40,50);
             }else{
-                image(milk,(i-10)*50+60,350,20,30);
+                image(milk,(i-10)*50+60,350,40,50);
             }
         }
-    }
-    
+
+
+        var button=createButton('Add Food');
+        button.position(30,30);
+        button.style("border-radius:20px;background-color:blue;color:white");
+        button.mousePressed(function(){
+            if(petposition != undefined && foodN<20){
+                reference.set(
+                    {
+                        food:foodN+1
+                    }
+                );
+                pet.changeAnimation("pet",petImg);
+                game="wait";
+            }
+            
+        });
+       
+
+        var button2=createButton('Feed the Food');
+        button2.position(150,30);
+        button2.style("border-radius:20px;background-color:blue;color:white");
+        button2.mousePressed(function(){if(petposition != undefined && game=="play") {
+            var now=new Date();
+            timeN=now.getHours();
+            if(timeN>12){
+                timeN=timeN-12;
+                a="PM";
+            }else{
+                a="AM";
+            }
+            console.log(timeN);
+            reference.set(
+                {
+                    food:foodN-1,
+                    time:timeN,
+                    time2:a
+                }
+            );
+            pet.changeAnimation("pet",petImg);
+            game="wait";
+            
+        }})
+
+    }  
+
 }
